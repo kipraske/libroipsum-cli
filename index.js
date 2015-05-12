dba021882de69cfb3f8402613a47d5809336bc12
Kristofer Raske <kristofer.raske@wustl.edu>
2015-05-12 16:09:57 -0500

    Adding in the yargs for our number of words and our key length

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 var LibroIpsum = require('libroipsum');
-var argv = require('yargs').argv;
+var argv = require('yargs')
+		.usage('Usage: $0 <sourcetext> [options]')
+		.demand(1)
+		.options('w', {
+			demand : true,
+			alias : 'words',
+			nargs : 1,
+			describe : 'Number of words to be generated'
+		})
+		.options('k', {
+			demand: false,
+			alias: 'key-length',
+			nargs: 1,
+			describe: 'Length of key (integer), where larger number will create phrase more similar to original text (optional, defaults to 6)'
+		})
+		.argv;
 
-var returnText = new LibroIpsum("Source Text is here").generate(26, 2);
+var returnText = new LibroIpsum("Source text goes here eventually").generate(argv.w, argv.k);
 console.log(returnText);
